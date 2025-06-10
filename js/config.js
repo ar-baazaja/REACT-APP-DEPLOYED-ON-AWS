5a9f57e894facecedd986759211415fd9378de0e
Arbaz Ahmed Awan <arbazawan2004@gmail.com>
2025-06-10 09:35:38 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_tzy2OKfMz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6184jvtsqv6g6qma7c5jugc52n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6ou94mku42loc2hkjsn5ogck1p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1g2j0el5jqn9d06722nv2ises7oar00gtfgl0h00ioetna6ctgmj',
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
